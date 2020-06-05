d2bfd5feabb1869e2fe74c9701d7a12927f4929b
Sean Murray <sean.j.murray1@gmail.com>
2020-06-05 15:19:37 -0700

    fix link

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "https://sean-secret.herokuapp.com//auth/google/secrets",
+    callbackURL: "https://sean-secret.herokuapp.com/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
